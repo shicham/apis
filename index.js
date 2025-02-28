11f9c9ed6a11bda2e8674784047b1962eb0e9001
shicham <sass.hicham@gmail.com>
2025-02-28 14:11:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());  // For parsing JSON bodies
-console.log(process.env.DATABASE_URL)
+console.log(process.env.MONGODB_URI)
 // MongoDB connection setup
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true
 })
 .then(() => console.log("MongoDB connected"))
@@ -40,7 +40,7 @@ app.post("/api/wishlist", async (req, res) => {
 app.get("/api/wishlist/", async (req, res) => {
   
     try {
-      res.json({wishlistItems:"11sssswishlistItems", url:process.env.DATABASE_URL});
+      res.json({wishlistItems:"11sssswishlistItemss", url:process.env.MONGODB_URI});
     } catch (error) {
       console.error("Error fetching wishlist:", error);
       res.status(500).json({ error: "Failed to fetch wishlist" });
